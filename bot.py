9e8bbf4f5ac0239daaca79e819a63dd9b0757ae6
Paul Cutler <paul.r.cutler@gmail.com>
2020-03-24 10:30:33 -0500

    Fix line breaks to be PEP8

--- bot.py
+++ bot.py
@@ -8,8 +8,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print(f"{client.user} has connected to Dicord.")
+    print(f"{client.user} has connected to Discord.")
 
 client.run(TOKEN)
